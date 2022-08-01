7e2fce55afc1d50a7b8d86f9a6782229354e51b6
Toni Lubiniecki (Z590E) <toni@lubiniecki.de>
2022-08-01 20:20:23 +0200

    windows added, tests fixed

--- tests/test_secnearios.py
+++ tests/test_secnearios.py
@@ -4,32 +4,40 @@ from vmc.simulation import SteerRamp
 import numpy as np
 
 
+def check_float_equality(x, y):
+    """Check floats for equality based on machine epsilon."""
+    return abs(x-y) < np.finfo(float).eps
+
+
 def test_SteerRamp():
     """Test functionality of open loop SteerRamp scenario."""
     t_start = 1
     t_end = 3
-    dt = 1
+    dt = 0.1
     dv = np.deg2rad(1)
     dvp = np.deg2rad(1)
 
     steer_ramp = SteerRamp(dt=dt, t_start=t_start, t_end=t_end,
-                           delta_v_max=dv, delta_vp=dvp)
+                           delta_v_max=dv, delta_vp=dvp, derivative=False)
 
-    assert steer_ramp.eval(t_start, False)[0] == 0.0
-    assert steer_ramp.eval(t_start, False)[1] == 0.0
+    assert steer_ramp.eval(t_start)[0] == 0.0
+    assert steer_ramp.eval(t_start)[1] == 0.0
 
-    assert steer_ramp.eval(t_start+dt, False)[0] == dv
-    assert steer_ramp.eval(t_start+dt, False)[1] == 0.0
+    assert steer_ramp.eval(t_start+dt)[0] > 0.0
+    assert steer_ramp.eval(t_start+dt)[1] == 0.0
 
-    assert steer_ramp.eval(t_end+dt, False)[0] == dv
-    assert steer_ramp.eval(t_end+dt, False)[1] == 0.0
+    assert check_float_equality(steer_ramp.eval(t_start+dv/dvp)[0], dv)
+    assert steer_ramp.eval(t_start+dv/dvp)[1] == 0.0
+
+    assert steer_ramp.eval(t_end)[0] == dv
+    assert steer_ramp.eval(t_end)[1] == 0.0
 
 
 def test_SteerRamp_derivative():
     """Test functionality of open loop SteerRamp scenario."""
     t_start = 1
     t_end = 3
-    dt = 1
+    dt = 0.1
     dv = np.deg2rad(1)
     dvp = np.deg2rad(1)
 
@@ -42,8 +50,8 @@ def test_SteerRamp_derivative():
     assert steer_ramp.eval(t_start+dt)[0] > 0.0
     assert steer_ramp.eval(t_start+dt)[1] == 0.0
 
-    assert steer_ramp.eval(t_end-dt)[0] > 0
-    assert steer_ramp.eval(t_end-dt)[1] == 0
+    assert check_float_equality(steer_ramp.eval(t_start+dv/dvp)[0], dvp)
+    assert steer_ramp.eval(t_start+dv/dvp)[1] == 0.0
 
     assert steer_ramp.eval(t_end)[0] == 0.0
     assert steer_ramp.eval(t_end)[1] == 0.0
